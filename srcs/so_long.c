7d312f3ab0db1ed32d20253315a6ad65725d322a
SuPuHe <omizin@42heilbronn.de>
2025-05-15 15:13:29 +0200

    loaded all imgs to my map and add some movement to player

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,7 +6,7 @@
 /*   By: omizin <omizin@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 10:18:20 by omizin            #+#    #+#             */
-/*   Updated: 2025/05/14 14:18:25 by omizin           ###   ########.fr       */
+/*   Updated: 2025/05/15 15:11:49 by omizin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ void	img_loading(t_map *map, int i, int j)
 
 	x = j * PIXEL;
 	y = i * PIXEL;
+	mlx_image_to_window(map->mlx, map->floor, x, y);
 	if (map->map[i][j] == 'P')
 	{
 		mlx_image_to_window(map->mlx, map->player_img, x, y);
 	}
-	// if (map->map[i][j] == '0')
-	// 	mlx_image_to_window(map->mlx, map->floor, x, y);
-	else
+	else if (map->map[i][j] == '1')
 	{
 		mlx_image_to_window(map->mlx, map->wall, x, y);
 	}
+	else if (map->map[i][j] == 'C')
+		mlx_image_to_window(map->mlx, map->collectible, x, y);
+	else if (map->map[i][j] == 'E')
+		mlx_image_to_window(map->mlx, map->exit_img, x, y);
 	// else if (map->map[i][j] == 'C')
 	// {
 	// 	mlx_image_to_window(map->mlx, map->collectible, x, y);
@@ -52,7 +55,25 @@ int	map_render(t_map *map)
 
 	texture.player = mlx_load_png("textures/player_imresizer.png");
 	texture.wall = mlx_load_png("textures/wall_imresizer.png");
+	texture.floor = mlx_load_png("textures/floor_imresizer.png");
+	texture.coin = mlx_load_png("textures/coin_imresizer.png");
+	texture.exit = mlx_load_png("textures/player_imresizer_exit.png");
+	if (!texture.coin)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
+	if (!texture.exit)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
 	if (!texture.player)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
+	if (!texture.floor)
 	{
 		mlx_terminate(map->mlx);
 		return (1);
@@ -66,12 +87,33 @@ int	map_render(t_map *map)
 	mlx_delete_texture(texture.player);
 	map->wall = mlx_texture_to_image(map->mlx, texture.wall);
 	mlx_delete_texture(texture.wall);
+	map->floor = mlx_texture_to_image(map->mlx, texture.floor);
+	mlx_delete_texture(texture.floor);
+	map->collectible = mlx_texture_to_image(map->mlx, texture.coin);
+	mlx_delete_texture(texture.coin);
+	map->exit_img = mlx_texture_to_image(map->mlx, texture.exit);
+	mlx_delete_texture(texture.exit);
 	if (!map->player_img)
 	{
 		mlx_terminate(map->mlx);
 		return (1);
 	}
+	if (!map->exit_img)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
+	if (!map->coin)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
 	if (!map->wall)
+	{
+		mlx_terminate(map->mlx);
+		return (1);
+	}
+	if (!map->floor)
 	{
 		mlx_terminate(map->mlx);
 		return (1);
@@ -95,8 +137,15 @@ void	move_player(t_map *map, int x, int y)
 {
 	if (map->map[map->player_y + y][map->player_x + x] == '1')
 		return ;
-	map->player_x += x;
-	map->player_y += y;
+	if (map->map[map->player_y][map->player_x] == 'E')
+		mlx_image_to_window(map->mlx, map->exit_img, map->player_x * PIXEL, map->player_y * PIXEL);
+	else
+		mlx_image_to_window(map->mlx, map->floor, map->player_x * PIXEL, map->player_y * PIXEL);
+	map->player_x = map->player_x + x;
+	map->player_y = map->player_y + y;
+	if (map->map[map->player_y][map->player_x] != 'E')
+		map->map[map->player_y][map->player_x] = 'P';
+	mlx_image_to_window(map->mlx, map->player_img, map->player_x * PIXEL, map->player_y * PIXEL);
 	ft_printf("PLAYER x: %d, y: %d\n", map->player_x, map->player_y);
 }
 
@@ -137,6 +186,8 @@ int	main(int argc, char **argv)
 		mlx_delete_image(map->mlx, map->player_img);
 	if (map->wall)
 		mlx_delete_image(map->mlx, map->wall);
+	if (map->floor)
+		mlx_delete_image(map->mlx, map->floor);
 	if (map->mlx)
 		mlx_terminate(map->mlx);
 	cleanup_map(map);
